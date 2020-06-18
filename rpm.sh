8c04f9eb4e15c7637004517abe8d0b21d3f1ee7a
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-18 21:56:54 +0500

    update

--- rpm.sh
+++ rpm.sh
@@ -15,8 +15,12 @@ Type old location for example if your movies were at /mnt/unionfs/movies just ty
 paths
 
 while [[ ! -d "/pg/unity/$folder_path" ]]; do 
-echo "folder $folder_path does not exist !" && paths 
+echo
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ⚠️ Folder $folder_path does not Exist! "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" && paths
 done
+
 
 sudo sqlite3 "/pg/data/$arr_name/$arr_name.db" "UPDATE RootFolders SET Path = '/pg/unity/$folder_path/' WHERE Path = '/mnt/unionfs/$folder_path/'"
 
@@ -27,9 +31,17 @@ read -p "Do you want to change the root paths for your Plex Libraries Y/N:" answ
 
 
 if [[ "$answer" == "Y" || "$answer" == "y" ]]; then 
-sudo sqlite3 "/pg/data/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "UPDATE media_parts SET file= replace(file, '/mnt/unionfs/', '/pg/unity/') where file like '%/mnt/unionfs/%'"; else 
-echo "Library Paths Unchanged" 
+sudo sqlite3 "/pg/data/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "UPDATE media_parts SET file= replace(file, '/mnt/unionfs/', '/pg/unity/') where file like '%/mnt/unionfs/%'" && echo
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ✅ Library Paths Changed "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+; else 
+echo
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ⚠️ Library Paths Unchanged "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 fi
+
 
 #DOCKER START CONTAINERS
 sudo docker start $(docker ps -a -q)
