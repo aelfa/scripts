8a702384835c15a2b5cf39a838a3dc07646af6b5
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-18 20:25:23 +0500

    update

--- rpm.sh
+++ rpm.sh
@@ -5,7 +5,15 @@
 
 #####################################################
 
+#####################################################
+# Stop all docker containers
+#####################################################
+
 sudo docker stop $(docker ps -a -q)
+
+#####################################################
+# Changed *arr Database Paths
+#####################################################
 
 paths() { read -p "
 Type old location for example if your movies were at /mnt/unionfs/movies just type movies:"  folder_path </dev/tty && read -p " type radarr or sonarr: " arr_name </dev/tty
@@ -19,13 +27,18 @@ done
 
 sudo sqlite3 "/pg/data/$arr_name/$arr_name.db" "UPDATE RootFolders SET Path = '/pg/unity/$folder_path/' WHERE Path = '/mnt/unionfs/$folder_path/'"
 
+#################################################
+# Change Plex Library Paths
+#################################################
+
 read -p "Do you want to change the root paths for your Plex Libraries Y/N:" answer </dev/tty
 
 if [[ $answer='Y' || $answer='y' ]];
 then sudo sqlite3 "/pg/data/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "UPDATE media_parts SET file= replace(file, '/mnt/unionfs/', '/pg/unity/') where file like '%/mnt/unionfs/%'"
-elif [[ $answer='N' || $answer='n' ]]
-then echo "Plex Library paths Unchanged"
+else echo "Library Paths Unchanged"
 fi
-
+##################################################
+# Start docker containers
+##################################################
 sudo docker start $(docker ps -a -q)
 
