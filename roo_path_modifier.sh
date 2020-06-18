83e5272ee21b406c1378cd7c88712a614a008b3e
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-18 18:19:01 +0500

    update

--- roo_path_modifier.sh
+++ roo_path_modifier.sh
@@ -3,11 +3,14 @@
 # script by ALPHA
 #####################################################
 
-sudo docker stop sonarr && sudo docker stop radarr
+#####################################################
+
+sudo docker stop $(docker ps -a -q)
 
 paths() { read -p "
 Type old location for example if your movies were at /mnt/unionfs/movies just type movies:"  folder_path </dev/tty && read -p " type radarr or sonarr: " arr_name </dev/tty
 }
+
 paths
 
 while [[ ! -d "/pg/unity/$folder_path" ]]; 
@@ -16,4 +19,8 @@ done
 
 sudo sqlite3 "/pg/data/$arr_name/$arr_name.db" "UPDATE RootFolders SET Path = '/pg/unity/$folder_path/' WHERE Path = '/mnt/unionfs/$folder_path/'"
 
-sudo docker start sonarr && sudo docker start radarr
+read -p "Do you want to change the root paths for your Plex Libraries Y/N:" answer </dev/tty
+while [[ ! -d "Y"]];
+do sudo sqlite3 "/pg/data/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "UPDATE media_parts SET file= replace(file, '/mnt/unionfs/', '/pg/unity/') where file like '%/mnt/unionfs/%'"
+done 
+
