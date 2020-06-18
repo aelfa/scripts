c422c5d80167eadf2ebeef985cc87df4b8b4460a
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-18 19:50:00 +0500

    update

--- roo_path_modifier.sh
+++ roo_path_modifier.sh
@@ -21,7 +21,9 @@ sudo sqlite3 "/pg/data/$arr_name/$arr_name.db" "UPDATE RootFolders SET Path = '/
 
 read -p "Do you want to change the root paths for your Plex Libraries Y/N:" answer </dev/tty
 
-while [[ ! -d "Y"]];
-do sudo sqlite3 "/pg/data/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "UPDATE media_parts SET file= replace(file, '/mnt/unionfs/', '/pg/unity/') where file like '%/mnt/unionfs/%'"
-done 
+if [[ $answer='Y' || $answer='y' ]];
+then sudo sqlite3 "/pg/data/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "UPDATE media_parts SET file= replace(file, '/mnt/unionfs/', '/pg/unity/') where file like '%/mnt/unionfs/%'"
+elif [[ $answer='N' || $answer='n' ]]
+then exit 
+fi
 
