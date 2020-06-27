93256fbb1eadb82ddb95cf110c66f352d2f4e575
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-28 01:38:16 +0500

    update

--- plex_change_date.sh
+++ plex_change_date.sh
@@ -3,13 +3,10 @@
 # script by ALPHA
 #####################################################
 
-PLEX_PATHS () {
-sudo docker inspect plex | grep config:rw | sed 's/\"//g' | tr -d ' ' | sed 's/\:.*//g' 2>&1 | tee /tmp/plex.info
+`sudo docker inspect plex | grep config:rw | sed 's/\"//g' | tr -d ' ' | sed 's/\:.*//g' 2>&1 | tee /tmp/plex.info`
 PLEX_ROOT="$(cat /tmp/plex.info)"
 PLEX_PREFERENCES="$(PLEX_ROOT)/Library/"Application Support"/"Plex Media Server"/Preferences.xml"
 PLEX_DATABASE="$(PLEX_ROOT)/Library/"Application Support"/"Plex Media Server"/"Plug-in Support"/Databases/com.plexapp.plugins.library.db"
-}
-PLEX_PATHS
 
 read -ep 'Modify Plex Library Dates to Original | [Y/N]: '  answer
 
