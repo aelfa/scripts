84e9c1bc6b466a931abcf898c390c42642d5757b
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-28 02:08:00 +0500

    update

---
+++ change_root_paths_arrs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#####################################################
+# script by ALPHA
+#####################################################
+
+#STOP ALL DOCKER CONTAINERS
+sudo docker stop $(docker ps -a -q)
+
+
+#ROOT PATH *ARR
+
+    ARR_PATHS () { read -ep "
+Type old location for example if your movies were at /mnt/unionfs/movies just type movies: "  folder_path && read -ep " type radarr or sonarr: " arr_name
+}
+
+ARR_PATHS
+
+while [[ ! -d "/pg/unity/$folder_path" ]]; do
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ⚠️ Folder $folder_path does not Exist! "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" && ARR_PATHS;
+done
+
+
+sudo sqlite3 "/pg/data/$arr_name/$arr_name.db" "UPDATE RootFolders SET Path = '/pg/unity/$folder_path/' WHERE Path = '/mnt/unionfs/$folder_path/'"
+
+#DOCKER START CONTAINERS
+sudo docker start $(docker ps -a -q)
+
+
---
+++ change_root_paths_plex.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+sudo docker stop plex
+sudo docker inspect plex | grep config:rw | sed 's/\"//g' | tr -d ' ' | sed 's/\:.*//g' 2>&1 | tee /tmp/plex.info
+PLEX_ROOT="$(cat /tmp/plex.info)"
+PLEX_PREFERENCES="${PLEX_ROOT}/Library/Application Support/Plex Media Server/Preferences.xml"
+PLEX_DATABASE="${PLEX_ROOT}/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
+
+read -ep 'Do you want to change Plex Library Paths | [Y/N]: '  answer
+
+if 
+    [ "${answer}" == "y" ] || [ "${answer}" == "Y" ] || [ "${answer}" == "yes" ] || [ "${answer}" == "Yes" ] || [ "${answer}" == "YES" ]; then
+    read -ep 'OLD MERGERFS DIRECTORY | [EXAMPLE:/mnt/unionfs/]: ' OLD_MERGERFS_LOCATION
+    read -ep 'NEW MERGERFS DIRECTORY | [EXAMPLE:/pg/unity/]: ' NEW_MERGERFS_LOCATION
+    sudo sqlite3 "${PLEX_DATABASE}" "UPDATE media_parts SET file= replace(file, '${OLD_MERGERFS_LOCATION}', '${NEW_MERGERFS_LOCATION}') where file like '%${OLD_MERGERFS_LOCATION}%'" &&
+    echo
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ✅ Library Paths Changed "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"; else 
+    echo
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ⚠️ Library Paths Unchanged "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+fi
+sudo docker start plex
