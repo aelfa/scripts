3bc66143ea212b04d032262f8d92bb551fd6da6f
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-28 01:15:49 +0500

    update

--- plex_change_date.sh
+++ plex_change_date.sh
@@ -6,7 +6,7 @@
 PLEX_PATHS () {
 sudo docker inspect plex | grep config:rw | sed 's/\"//g' | tr -d ' ' | sed 's/\:.*//g' 2>&1 | tee /tmp/plex.info
 PLEX_ROOT=$(cat /tmp/plex.info)
-PLEX_PREFERENCES=$(PLEX_APPDATA_ROOT)/Library/Application Support/Plex Media Server/Preferences.xml)
+PLEX_PREFERENCES=$(PLEX_APPDATA_ROOT)/Library/Application Support/Plex Media Server/Preferences.xml
 PLEX_DATABASE=$(PLEX_APPDATA_ROOT)/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db
 }
 
