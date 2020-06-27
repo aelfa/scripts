2397b497410b655df90b64756d6f3853ac849c82
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-28 01:21:00 +0500

    update

--- plex_change_date.sh
+++ plex_change_date.sh
@@ -9,24 +9,24 @@ PLEX_ROOT=$(cat /tmp/plex.info)
 PLEX_PREFERENCES=$(PLEX_APPDATA_ROOT)/Library/Application Support/Plex Media Server/Preferences.xml
 PLEX_DATABASE=$(PLEX_APPDATA_ROOT)/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db
 }
+PLEX_PATHS
 
 read -ep 'Do you want to change Plex Library Paths | [Y/N]: '  answer
 
 if 
     [ "${answer}" == "y" ] || [ "${answer}" == "Y" ] || [ "${answer}" == "yes" ] || [ "${answer}" == "Yes" ] || [ "${answer}" == "YES" ]; 
 then
-    [ PLEX_PATHS ] && [ sudo sqlite3 "${PLEX_DATABASE}" "UPDATE metadata_items SET created_at = originally_available_at WHERE DATETIME(created_at) > DATETIME('now');" ] && [ sudo sqlite3 "${PLEX_DATABASE}" "UPDATE metadata_items SET added_at = originally_available_at WHERE DATETIME(added_at) > DATETIME('now');" ] && 
+    [ sudo sqlite3 "${PLEX_DATABASE}" "UPDATE metadata_items SET created_at = originally_available_at WHERE DATETIME(created_at) > DATETIME('now');" ] && [ sudo sqlite3 "${PLEX_DATABASE}" "UPDATE metadata_items SET added_at = originally_available_at WHERE DATETIME(added_at) > DATETIME('now');" ] && 
     [                                 
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     echo " ✅ Library Dates Changed to Original "
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    ];
-else 
-    [
+    ]; 
+else
+    echo 
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     echo " ⚠️ No Changes Made "
-    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    ];
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
 fi
 
 
