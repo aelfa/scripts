406dff8814c3e088890f29c915c1e5e424428be4
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-28 23:12:22 +0500

    UPDATE

--- change_root_paths_plex.sh
+++ change_root_paths_plex.sh
@@ -1,7 +1,5 @@
-
 #!/bin/bash
 
-sudo docker stop plex
 sudo docker inspect plex | grep config:rw | sed 's/\"//g' | tr -d ' ' | sed 's/\:.*//g' 2>&1 | tee /tmp/plex.info
 PLEX_ROOT="$(cat /tmp/plex.info)"
 PLEX_PREFERENCES="${PLEX_ROOT}/Library/Application Support/Plex Media Server/Preferences.xml"
@@ -11,6 +9,7 @@ read -ep 'Do you want to change Plex Library Paths | [Y/N]: '  answer
 
 if 
     [ "${answer}" == "y" ] || [ "${answer}" == "Y" ] || [ "${answer}" == "yes" ] || [ "${answer}" == "Yes" ] || [ "${answer}" == "YES" ]; then
+    sudo docker stop plex
     read -ep 'OLD MERGERFS DIRECTORY | [EXAMPLE:/mnt/unionfs/]: ' OLD_MERGERFS_LOCATION
     read -ep 'NEW MERGERFS DIRECTORY | [EXAMPLE:/pg/unity/]: ' NEW_MERGERFS_LOCATION
     sudo sqlite3 "${PLEX_DATABASE}" "UPDATE media_parts SET file= replace(file, '${OLD_MERGERFS_LOCATION}', '${NEW_MERGERFS_LOCATION}') where file like '%${OLD_MERGERFS_LOCATION}%'" &&
