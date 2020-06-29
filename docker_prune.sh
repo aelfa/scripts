1df3900643477e9519022540966f28d03edb9cf9
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-29 20:11:19 +0500

    update

---
+++ docker_prune.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -ep 'Would you like to remove all unused containers, networks, volumes, images and build cache? | [Y/N]: '  answer
+
+if 
+    [ "${answer}" == "y" ] || [ "${answer}" == "Y" ] || [ "${answer}" == "yes" ] || [ "${answer}" == "Yes" ] || [ "${answer}" == "YES" ]; then
+    sudo docker system prune -a --volumes --force || error "Failed to remove unused docker resources."; else
+    echo 
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo " ⚠️ No Changes Made "
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+fi
