75c7a5872749cb47903ebe45e421d2b4d3698171
ΔLPHΔ <60222501+Dope-Alpha@users.noreply.github.com>
2020-06-29 20:55:18 +0500

    UPDATE

--- change_root_paths_plex.sh
+++ change_root_paths_plex.sh
@@ -5,7 +5,6 @@ MERGERFS_PATH () {
     read -ep 'NEW MERGERFS DIRECTORY | [EXAMPLE:/pg/unity/]: ' NEW_MERGERFS_LOCATION
 if [[ ! -d "${NEW_MERGERFS_LOCATION}" ]]; then
     echo
-    
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     echo " ⚠️ ${NEW_MERGERFS_LOCATION} is not a valid mergerfs path "
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" || [ MERGERFS_PATH ];
