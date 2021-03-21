993160ed84e18c18a9f1f0249ab24700d7cdcbe6
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-03-21 23:56:56 +0500

    Update doob187.sh

--- doob187.sh
+++ doob187.sh
@@ -10,17 +10,33 @@ EOF
   fi
 }
 function clonetraefik () {
-    sudo apt install git -yy
-    sudo git clone --quiet https://github.com/doob187/Traefikv2 /opt/traefik
-    sudo chown -cR 1000:1000 /opt/traefik/ 1>/dev/null 2>&1
-    sudo chmod -cR 755 /opt/traefik >> /dev/null 1>/dev/null 2>&1
-    sudo bash /opt/traefik/install.sh
+      if [[ ! -d "/opt/traefik" ]]; then
+            sudo apt install git -yy
+            sudo git clone --quiet https://github.com/doob187/Traefikv2 /opt/traefik
+            sudo chown -cR 1000:1000 /opt/traefik/ 1>/dev/null 2>&1
+            sudo chmod -cR 755 /opt/traefik >> /dev/null 1>/dev/null 2>&1
+            sudo bash /opt/traefik/install.sh
+    else
+        git -C "/opt/traefik" pull
+        git -C "/opt/traefik" fetch --all --prune
+        git -C "/opt/traefik" reset --hard origin/master
+        git -C "/opt/traefik" pull
+        sudo bash /opt/traefik/install.sh
+    fi
 }
 function cloneapps () {
+  if [[ ! -d "/opt/apps" ]]; then
     sudo git clone --quiet https://github.com/doob187/traefikv2apps /opt/apps
     sudo chown -cR 1000:1000 /opt/apps/ 1>/dev/null 2>&1
     sudo chmod -cR 755 /opt/apps >> /dev/null 1>/dev/null 2>&1
-    sudo bash /opt/apps/install.sh
+    sudo bash /opt/apps/install.sh 
+  else
+        git -C "/opt/apps" pull
+        git -C "/opt/apps" fetch --all --prune
+        git -C "/opt/apps" reset --hard origin/master
+        git -C "/opt/apps" pull
+        sudo bash /opt/apps/install.sh
+  fi
 }
 function main () {
     echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
