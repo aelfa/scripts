92ca80da4c20df99d91e00e699848dd8874dfa8f
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-04-04 14:03:36 +0500

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -10,5 +10,7 @@ read -ep "Destination containing git repo to update [ENTER]: " answer
     $(command -v git) -C "${answer}" fetch --all --prune
     $(command -v git) -C "${answer}" reset --hard origin/master
     $(command -v git) -C "${answer}" pull
-}
+    sudo "$(command -v chown)" -cR 1000:1000 "${answer}" 1>/dev/null 2>&1
+    sudo "$(command -v chmod)" -cR 755 "${answer}" 1>/dev/null 2>&1 
+ }
 update
