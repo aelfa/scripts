05e8a3660f2b451bc55d5ccddd77b90ffa4877fc
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-04-04 15:05:47 +0500

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -1,13 +1,20 @@
-# shellcheck shell=bash
+#!/bin/bash
 # Copyright (c) 2020, Aelfa
 # All rights reserved.
-# shellcheck disable=SC2086
-# shellcheck disable=SC2006
 # Useful script to update already cloned git repos
-read -ep "Destination containing git repo to update [ENTER]: " answer  
-    $(command -v git) -C "${answer}" pull
-    $(command -v git) -C "${answer}" fetch --all --prune
-    $(command -v git) -C "${answer}" reset --hard origin/master
-    $(command -v git) -C "${answer}" pull
-    sudo "$(command -v chown)" -cR 1000:1000 "${answer}" 1>/dev/null 2>&1
-    sudo "$(command -v chmod)" -cR 755 "${answer}" 1>/dev/null 2>&1 
+
+paths() {
+    read  -r -ep 'ENTER repo location | [EXAMPLE:/opt/scripts/test]: ' repo_path
+    if [[ ! -f "${repo_path}/.git" ]]; then
+    echo " ⚠️ ${repo_path} is not a git repo location " && paths;
+    fi
+}
+
+update() {
+    sudo "$(command -v git)" -C "${repo_path}" pull
+    sudo "$(command -v git)" -C "${repo_path}" fetch --all --prune
+    sudo "$(command -v git)" -C "${repo_path}" reset --hard HEAD
+    sudo "$(command -v git)" -C "${repo_path}" pull
+}
+paths
+update
