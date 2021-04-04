97a9e8661efe436987a47c74c117fbddf875b4e1
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-04-04 20:04:56 +0500

    Update githubclone.sh

--- githubclone.sh
+++ githubclone.sh
@@ -7,12 +7,12 @@
     REPO_PREFIX="https://github.com"
     REPO_BASE=/opt/data # Edit this to change the base of your repos
     echo " ⚠️ The script assumes the repos are located in a base location "
-    read -rp 'Author of the repo | ⚠️ CASE SENSITIVE | [EXAMPLE:aelfa]: ' GIT_USER
+    read -rp 'Owner of the repo | ⚠️ CASE SENSITIVE | [EXAMPLE:aelfa]: ' GIT_USER
     read -rp 'Name of the repo | ⚠️ CASE SENSITIVE | [EXAMPLE:scripts]: ' GIT_REPO
     REPO_NAME=${GIT_REPO}
-    REPO_AUTHOR=${GIT_USER}
-    REPO_PATH=${REPO_BASE}/${REPO_AUTHOR}/${REPO_NAME}
-    REPO_LINK=${REPO_PREFIX}/${REPO_AUTHOR}/${REPO_NAME}.git
+    REPO_OWNER=${GIT_USER}
+    REPO_PATH=${REPO_BASE}/${REPO_OWNER}/${REPO_NAME}
+    REPO_LINK=${REPO_PREFIX}/${REPO_OWNER}/${REPO_NAME}.git
     
 # CLONE FUNCTION
 clone () {
