aa49a81d87a639a97e7fb9f50dc262f764f66716
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-04-04 14:09:17 +0500

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -4,7 +4,6 @@
 # shellcheck disable=SC2086
 # shellcheck disable=SC2006
 # Useful script to update already cloned git repos
-update() {
 read -ep "Destination containing git repo to update [ENTER]: " answer  
     $(command -v git) -C "${answer}" pull
     $(command -v git) -C "${answer}" fetch --all --prune
@@ -12,5 +11,3 @@ read -ep "Destination containing git repo to update [ENTER]: " answer
     $(command -v git) -C "${answer}" pull
     sudo "$(command -v chown)" -cR 1000:1000 "${answer}" 1>/dev/null 2>&1
     sudo "$(command -v chmod)" -cR 755 "${answer}" 1>/dev/null 2>&1 
-}
-update
