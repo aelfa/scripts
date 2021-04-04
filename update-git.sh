67b72d6763002f0892d724887196bd120c758953
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-04-04 14:00:29 +0500

    Create update-git.sh

---
+++ update-git.sh
@@ -0,0 +1,14 @@
+# shellcheck shell=bash
+# Copyright (c) 2020, Aelfa
+# All rights reserved.
+# shellcheck disable=SC2086
+# shellcheck disable=SC2006
+# Useful script to update already cloned git repos
+update () {
+read -ep "Destination containing git repo to update [ENTER]: " answer  
+    $(command -v git) -C "${answer}" pull
+    $(command -v git) -C "${answer}" fetch --all --prune
+    $(command -v git) -C "${answer}" reset --hard origin/master
+    $(command -v git) -C "${answer}" pull
+}
+update
