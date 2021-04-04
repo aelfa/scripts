1c6219dc98186baa4d978f6556f7b8c10c33aa9f
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-04-04 15:20:11 +0500

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -11,14 +11,14 @@ path() {
 }
 
 update() {
-    sudo '$(command -v git)' -C "${repo_path}" pull
-    sudo '$(command -v git)' -C "${repo_path}" fetch --all --prune
-    sudo '$(command -v git)' -C "${repo_path}" reset --hard HEAD
-    sudo '$(command -v git)' -C "${repo_path}" pull
+    sudo "$(command -v git)" -C "${repo_path}" pull
+    sudo "$(command -v git)" -C "${repo_path}" fetch --all --prune
+    sudo "$(command -v git)" -C "${repo_path}" reset --hard HEAD
+    sudo "$(command -v git)" -C "${repo_path}" pull
 }
 permissions() {
-    sudo '$(command -v chown)' -cR 1000:1000 "${repo_path}" 1>/dev/null 2>&1
-    sudo '$(command -v chmod)' -cR 755 "${repo_path}" 1>/dev/null 2>&1
+    sudo "$(command -v chown)" -cR 1000:1000 "${repo_path}" 1>/dev/null 2>&1
+    sudo "$(command -v chmod)" -cR 755 "${repo_path}" 1>/dev/null 2>&1
 }
 path
 permissions
