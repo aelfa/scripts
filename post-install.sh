4455b7dac6bf76c7d554bb6f356edbc1a9eb0c8c
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-08-13 13:53:47 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-##!Maintainer: Aelfa
-###!Adjusted and Made Public with love for @Hawks
-####!Original Maintainer nemchik
 set -euo pipefail
 IFS=$'\n\t'
 
@@ -9,7 +6,7 @@ IFS=$'\n\t'
 readonly DETECTED_PUID=${SUDO_UID:-$UID}
 readonly DETECTED_UNAME=$(id -un "${DETECTED_PUID}" 2> /dev/null || true)
 readonly DETECTED_PGID=$(id -g "${DETECTED_PUID}" 2> /dev/null || true)
-#readonly DETECTED_UGROUP=$(id -gn "${DETECTED_PUID}" 2> /dev/null || true)
+readonly DETECTED_UGROUP=$(id -gn "${DETECTED_PUID}" 2> /dev/null || true)
 readonly DETECTED_HOMEDIR=$(eval echo "~${DETECTED_UNAME}" 2> /dev/null || true)
 
 # Root Check Function
@@ -42,10 +39,10 @@ main() {
     fi
 
     # System Info
-    #readonly ARCH=$(uname -m)
-    #readonly DPKG_ARCH=$(dpkg --print-architecture)
-    #readonly ID=$(grep --color=never -Po '^ID=\K.*' /etc/os-release)
-    #readonly VERSION_CODENAME=$(grep --color=never -Po '^VERSION_CODENAME=\K.*' /etc/os-release)
+    readonly ARCH=$(uname -m)
+    readonly DPKG_ARCH=$(dpkg --print-architecture)
+    readonly ID=$(grep --color=never -Po '^ID=\K.*' /etc/os-release)
+    readonly VERSION_CODENAME=$(grep --color=never -Po '^VERSION_CODENAME=\K.*' /etc/os-release)
 
     # apt-get updates, installs, and cleanups
     sudo apt-get -y update
@@ -69,11 +66,10 @@ main() {
     sudo apt-get -y dist-upgrade
     sudo apt-get -y autoremove
     sudo apt-get -y autoclean
-    sudo pip3 freeze --local |sed -rn 's/^([^=# \t\\][^ \t=]*)=.*/echo; echo Processing \1 ...; sudo pip3 install -U \1/p' |sh
 
     # kernel modules for vpn
-    echo "iptable_mangle" | sudo tee /etc/modules-load.d/iptable_mangle.conf
-    echo "tun" | sudo tee /etc/modules-load.d/tun.conf
+    #echo "iptable_mangle" | sudo tee /etc/modules-load.d/iptable_mangle.conf
+    #echo "tun" | sudo tee /etc/modules-load.d/tun.conf
 
     # tmux config
     # https://github.com/gpakosz/.tmux
@@ -106,10 +102,12 @@ main() {
     if ! grep -q 'bashrc-tmux' "${DETECTED_HOMEDIR}/.bashrc"; then
         local BASHRC_TMP
         BASHRC_TMP=$(mktemp)
-      cat <<-'EOF' | sed -E 's/^ *//' | cat - "${DETECTED_HOMEDIR}/.bashrc" > "${BASHRC_TMP}"
+        cat <<- 'EOF' | sed -E 's/^ *//' | cat - "${DETECTED_HOMEDIR}/.bashrc" > "${BASHRC_TMP}"
             [ -z "$PS1" ] && return                 # this still comes first
             source ~/bashrc-tmux/bashrc-tmux
+
             # rest of bashrc below...
+
 EOF
         mv "${BASHRC_TMP}" "${DETECTED_HOMEDIR}/.bashrc"
         rm -f "${BASHRC_TMP}"
@@ -118,10 +116,10 @@ EOF
     chown -R "${DETECTED_PUID}":"${DETECTED_PGID}" "${DETECTED_HOMEDIR}/.bashrc"
 
     # https://help.ubuntu.com/community/StricterDefaults#Shared_Memory
-    if ! grep -q '/run/shm' /etc/fstab; then
-        echo "none     /run/shm     tmpfs     defaults,ro     0     0" >> /etc/fstab
-    fi
-    sudo mount -o remount /run/shm || true
+    #if ! grep -q '/run/shm' /etc/fstab; then
+        #echo "none     /run/shm     tmpfs     defaults,ro     0     0" >> /etc/fstab
+    #fi
+    #sudo mount -o remount /run/shm || true
 
     # https://help.ubuntu.com/community/StricterDefaults#SSH_Login_Grace_Time
     sudo sed -i -E 's/^#?LoginGraceTime .*$/LoginGraceTime 20/g' /etc/ssh/sshd_config
@@ -138,10 +136,13 @@ EOF
 
     # restart ssh after all the changes above
     sudo systemctl restart ssh
+
+    # https://github.com/trapexit/mergerfs/releases
+    #local AVAILABLE_MERGERFS
+    #AVAILABLE_MERGERFS=$(curl -fsL "https://api.github.com/repos/trapexit/mergerfs/releases/latest" | grep -Po '"tag_name": "[Vv]?\K.*?(?=")')
+    #local MERGERFS_FILENAME="mergerfs_${AVAILABLE_MERGERFS}.${ID}-${VERSION_CODENAME}_${DPKG_ARCH}.deb"
+    #curl -fsL "https://github.com/trapexit/mergerfs/releases/download/${AVAILABLE_MERGERFS}/${MERGERFS_FILENAME}" -o "${MERGERFS_FILENAME}"
+    #sudo dpkg -i "${MERGERFS_FILENAME}"
+    #rm -f "${MERGERFS_FILENAME}" || true
 }
-    if [[ ! -f "${DETECTED_HOMEDIR}/.ssh/authorized_keys" ]]; then
-        echo "authorized_keys file does not exist."
-        exit 1
-    else 
-        main
-    fi
+main
