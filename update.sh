f1c5b0694a61ebed1fc0ca8d5b3ce78b82a79c1a
Aelfa <aelfa@protonmail.com>
2021-08-31 04:01:20 +0200

    update

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ EOF
 updateall() {
 package_list="update upgrade dist-upgrade autoremove autoclean "
 for i in ${package_list}; do
-    sudo apt $i -yqq 1>/dev/null 2>&1
+    sudo apt "$i" -yqq 1>/dev/null 2>&1
     echo "$i is running , please wait"
     sleep 1
 done
--- useradd.sh
+++ useradd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ##usercheck
-if [ $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}') ]; then
-	usermod -aG sudo $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
-        sudo usermod -s /bin/bash $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
-        sudo usermod -aG video $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
-        sudo usermod -aG docker $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
+if [ "$(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')" ]; then
+	usermod -aG sudo "$(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')"
+        sudo usermod -s /bin/bash "$(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')"
+        sudo usermod -aG video "$(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')"
+        sudo usermod -aG docker "$(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')"
         echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
         echo ""
-        echo " ✅ PASSED ! We found the user UID " $(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')
+        echo " ✅ PASSED ! We found the user UID " "$(grep "1000" /etc/passwd | cut -d: -f1 | awk '{print $1}')"
         echo ""
         echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 else
@@ -21,18 +21,18 @@ else
         read -rp "Enter username : " username
         read -s -rp "Enter password : " password
         echo ""
-        egrep "^$username" /etc/passwd >/dev/null
-                pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-                useradd -m -p $pass $username
-                usermod -aG sudo $username
-                sudo usermod -s /bin/bash $username
-                usermod -aG video $username
-				usermod -aG docker $username
+        grep -E "^$username" /etc/passwd >/dev/null
+                pass=$(perl -e 'print crypt($ARGV[0], "password")' "$password")
+                useradd -m -p "$pass" "$username"
+                usermod -aG sudo "$username"
+                sudo usermod -s /bin/bash "$username"
+                usermod -aG video "$username"
+				usermod -aG docker "$username"
         echo ""
         echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
         echo " ✅ PASSED ! User has been added to system!"
-        echo " ✅ PASSED ! Your Username : " $username
-        echo " ✅ PASSED ! Your Password : " $password
+        echo " ✅ PASSED ! Your Username : " "$username"
+        echo " ✅ PASSED ! Your Password : " "$password"
         echo " ✅ PASSED ! "
         echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 fi
