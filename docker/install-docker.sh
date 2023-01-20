25552029d6aaf6c96deb1b4ff639de2c3ae2b58e
Aelfa <60222501+aelfa@users.noreply.github.com>
2023-01-20 21:08:40 +0500

    Update install-docker.sh

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 function startcommand() {
+    # Check if Docker is installed
     docker=$(docker --version)
     if [[ "$docker" == "" ]]; then
         install_docker
     else
-        reinstall_docker
+        update_docker
     fi
 }
 
-function reinstall_docker() {
+function update_docker() {
     tee <<-EOF
 ━━━━━━━━━━━━━━━━━━
-[Y] UPDATE to lateste version
+[Y] UPDATE to latest version
 [Z] Exit
 ━━━━━━━━━━━━━━━━━━
 EOF
@@ -28,16 +29,35 @@ EOF
 }
 
 function install_docker() {
-    sudo apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg2 \
-        software-properties-common
-
-    curl https://raw.githubusercontent.com/docker/docker-install/master/install.sh | sudo bash
-    sudo systemctl enable --now docker
-    sudo systemctl status docker | awk '$1 == "Active:" {print $2,$3}'
+    # Check the OS version
+    os=$(uname -s)
+    if [ "$os" == "Linux" ]; then
+        # Check the Linux distribution
+        distro=$(lsb_release -i -s)
+        if [ "$distro" == "Ubuntu" ]; then
+            # Install Docker on Ubuntu
+            sudo apt-get update
+            sudo apt-get install -y docker.io
+            sudo systemctl enable --now docker
+            sudo systemctl status docker | awk '$1 == "Active:" {print $2,$3}'
+        elif [ "$distro" == "CentOS" ]; then
+            # Install Docker on CentOS
+            sudo yum install -y yum-utils
+            sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+            sudo yum install -y docker-ce
+            sudo systemctl enable --now docker
+            sudo systemctl status docker | awk '$1 == "Active:" {print $2,$3}'
+        else
+            echo "This script does not support your Linux distribution."
+            exit 1
+        fi
+    elif [ "$os" == "Darwin" ]; then
+        # Install Docker on macOS
+        brew install docker
+    else
+        echo "This script does not support your OS."
+        exit 1
+    fi
 
     # add current user to docker group so there is no need to use sudo when running docker
     sudo usermod -aG docker "$(whoami)"
@@ -45,4 +65,3 @@ function install_docker() {
 }
 
 startcommand
-#<EOF>#
