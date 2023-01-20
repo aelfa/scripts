cab9cb9f4bb50d946bda2f53ba16ceabf0913dda
Aelfa <60222501+aelfa@users.noreply.github.com>
2023-01-20 21:10:56 +0500

    Update docker-prune.sh

--- docker/docker-prune.sh
+++ docker/docker-prune.sh
@@ -2,21 +2,23 @@
 
 function dockerprune() {
 
-    read -erp 'Would you like to remove all unused containers, networks, volumes, images and build cache? | [Y/N]: ' answer
+    echo "This script will remove all unused containers, networks, volumes, images and build cache."
+    echo "Are you sure you want to continue? [Y/N]"
+    read -r answer
 
-    if
-        [ "${answer}" == "y" ] || [ "${answer}" == "Y" ] || [ "${answer}" == "yes" ] || [ "${answer}" == "Yes" ] || [ "${answer}" == "YES" ]
-    then
-        sudo "$(which docker)" system prune -a --volumes --force &&
-            echo
-        echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-        echo " ✅ Prune Completed Successfully "
-        echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" || echo " ⚠️ Failed to remove unused docker resources."
+    if [[ "$answer" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+        # check if Docker is installed
+        if command -v docker >/dev/null 2>&1; then
+            sudo "$(which docker)" system prune -a --volumes --force
+            echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+            echo "✅ Prune Completed Successfully"
+            echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+        else
+            echo "Docker is not installed on this machine. Please install Docker and try again."
+            exit 1
+        fi
     else
-        echo
-        echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-        echo " ⚠️ Docker Prune was aborted "
-        echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+        echo "Aborting Docker Prune."
     fi
 }
 dockerprune
